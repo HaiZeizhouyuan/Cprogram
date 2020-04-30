53278dc1134516f534068409a3c9cf45137440af
Vincent <zy@hotmail,com>
2020-04-30 10:39:11 +0800

    OJ: 104 105 106

--- QJ/first.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int main(){
-	printf("Hello HaiZei\n");
-	printf("Hi");
-	printf("lll");
-	return 0;
-}
--- QJ/third.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-int main(){
-	printf("kkkkk\n");
-	return 0;
-}
