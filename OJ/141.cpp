a0e618b99f93df1905842fe3fc4d26fbfdc011fa
Vincent <zy@hotmail,com>
2020-05-03 22:53:46 +0800

    OJ

--- OJ/141.cpp
+++ OJ/141.cpp
@@ -11,7 +11,7 @@ int main(){
     int n;
     cin >> n;
     for (int i = 0; i <= n; i++) {
-        for (int j = 0; i <= i; j++) {
+        for (int j = 0; j < i; j++) {
             cout << " ";
         }
         for(int j = i; j <= n; j++){
